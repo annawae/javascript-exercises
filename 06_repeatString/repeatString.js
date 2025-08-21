f6886f44821f184fe6f5c3728f269570cea6399f
annawae <187255385+knuffelchen@users.noreply.github.com>
2025-08-21 04:43:21 +0200

    done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let newString= "";
+    if(number == 0){
+        return "Error";
+    }
+    else{
+    for(let i=0; i<number; i++){
+        newString = newString+word;
+    }
+    return newString;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
