eb6c375b2c1190147452ed28a6076aaf036412bd
annawae <187255385+knuffelchen@users.noreply.github.com>
2025-08-21 07:10:27 +0200

    done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if(a<b){
+        
+        for(let i=a; i<=b; i++){
+            sum = sum + i;
+        }
+        
+    }
+    else if(b<a){
+        for(let y=b; y<=a; y++){
+            sum = sum + y;
+        }
 
+    }
+    else sum= a+b;
+
+    return sum;
 };
 
 // Do not edit below this line
